96c1cbc13dfbc4a06000a881262347a5237b5fb4
zeynalovva <zeynalovvabbas@gmail.com>
2025-04-24 14:10:47 +0400

    search

---
+++ src/main/java/com/flightbooking/SearchEgnine/Search.java
@@ -0,0 +1,13 @@
+package com.flightbooking.SearchEgnine;
+
+import com.flightbooking.Ticket;
+
+import java.util.List;
+
+public class Search {
+
+
+    public List<Object> filterTickets(List<Ticket> data, String name, String surname){
+        List<Ticket> list;
+    }
+}
